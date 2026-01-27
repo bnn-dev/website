0750c3935c0ffffa67a6e6b3104ff70a676b3ed1
0xr3ngar <bogdan.nikolov4@Outlook.com>
2026-01-28 00:14:25 +0100

    Remove head/body tags from layout to fix hydration

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -1,21 +1,7 @@
 import '../styles.css';
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body>{children}</body>
-    </>
-  );
+  return children;
 }
 
 export const getConfig = async () => {
