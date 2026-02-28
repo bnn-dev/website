230cc560a350e8a63621d058d3de25772392300c
0xr3ngar <84232456+0xr3ngar@users.noreply.github.com>
2026-02-28 21:29:02 +0100

    feat: add SIFT project link and bnn favicon

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -5,6 +5,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       <link
