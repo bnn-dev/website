f5d94c510bbdf8ae21a6e81dfdb0001c42681831
0xr3ngar <bogdan.nikolov4@Outlook.com>
2026-02-28 21:37:33 +0100

    chore: remove swe seceng title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,6 @@ export default async function HomePage() {
             <div className="home-content">
                 <OrthodoxCross className="cross" />
                 <h1 className="name">Bogdan Nikolov</h1>
-                <div className="subtitle">
-                    <span className="subtitle-left">SWE</span>
-                    <span className="pipe">|</span>
-                    <span className="subtitle-right">SecEng</span>
-                </div>
                 <p className="quote">Simplicity is the ultimate sophistication</p>
                 <nav className="links">
                     <a href="https://github.com/0xr3ngar" className="link link-left" target="_blank" rel="noopener noreferrer">
